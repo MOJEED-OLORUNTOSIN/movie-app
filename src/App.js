2f655decfe508406f26125552d758b908c34a2ee
MOJEED-OLORUNTOSIN <mojeedoloruntosin@gmail.com>
2023-11-25 12:30:51 +0100

    my project commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import MovieCard from "./MovieCard";
 function App() {
+  const API_URL =
+    "https://api.themoviedb.org/3/movie/popular?api_key=df34659b47b67bafe6a31303c8ed1bca";
+  const API_SEARCH =
+    "https://api.themoviedb.org/3/search/movie?df34659b47b67bafe6a31303c8ed1bca&query=";
+  const [movies, setMovies] = useState([]);
+  const [term, setTerm] = useState("");
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.results));
+  }, []);
+
+  console.log(movies);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetch(
+      (API_SEARCH + term)
+        .then((res) => res.json())
+        .then((data) => setMovies(data.results))
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search_nav">
+        <div className="title">
+          <h1>Movies</h1>
+        </div>
+        <div className="search_box">
+          <form onSubmit={handleSearch}>
+            <input onChange={(e) => setTerm(e.target.value)} />
+            <button>search</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="movies">
+        {movies.map((movie) => (
+          <MovieCard {...movie} />
+        ))}
+      </div>
     </div>
   );
 }
